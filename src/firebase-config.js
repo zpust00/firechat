df5b973c7e802d648744b4674adb5a001faa9067
Gregor Kikelj <gregor1234567890@gmail.com>
2024-06-16 20:00:55 +0200

    login

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2eVlkFCXR_GbONXcTFhVBgzryrmViQxM",
+    authDomain: "zpust-firebase.firebaseapp.com",
+    projectId: "zpust-firebase",
+    storageBucket: "zpust-firebase.appspot.com",
+    messagingSenderId: "989594916389",
+    appId: "1:989594916389:web:b347108f2cb236a19fd1cc"
+  };
